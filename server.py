1ec56ac3e71d4b80b42655eae9bf7e7db4dc6096
rywit <witko.ryan@gmail.com>
2012-03-08 20:48:24 -0500

    Now output urls in JSON format

--- server.py
+++ server.py
@@ -1,20 +1,27 @@
 import Image
 import requests
 import cStringIO
-import os
+import json
 
 IMAGES_FOLDER = "retrieved_images"
 
-urls = [
+input_urls = [
 	"http://cdn-www.dailypuppy.com/images/widget_box_example.jpg",
 	"http://www.makems.com/graphic/puppies-2.jpg"
 ]
 
+output_urls = []
+
 size = 256, 256
 
 # TODO: make all of the requests for images happen at the same time
-for i, url in enumerate(urls):
+for i, url in enumerate( input_urls ):
 	r = requests.get(url)
 	im = Image.open(cStringIO.StringIO(r.content))
 	img = im.resize(size)
-	img.save(os.path.join(IMAGES_FOLDER, "image%d.jpg" % i))
+	
+	filename = IMAGES_FOLDER + "/image%d.jpg" % i
+	img.save( filename )
+	output_urls.append( filename )
+	
+print json.dumps( output_urls )
